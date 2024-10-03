6812b59da552f39fa82c32462472e761ad9df949
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-10-04 03:27:57 +0530

    Attempt:1

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+bands.sort()
+let container = document.querySelector("#band")
+for(let t of bands){
+    let x = document.createElement("li")
+    x.innerText = t;
+    container.append(x)
+}
