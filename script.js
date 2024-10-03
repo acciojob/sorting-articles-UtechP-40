feb1de59a3caa386fb79733d02c6ccff873ac3f8
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-10-04 03:44:34 +0530

    Attempt:3

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-//your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-bands.sort()
+const bands = [
+    'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 
+    'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 
+    'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 
+    'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+];
+
+bands.sort();
+
 let container = document.querySelector("#bands");
 
-for (let band of bands) {
+let limitedBands = bands.slice(0, 8);
+
+for (let band of limitedBands) {
     let li = document.createElement("li");
     li.innerText = band;
     container.append(li);
